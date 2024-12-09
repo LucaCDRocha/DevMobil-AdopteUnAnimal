54740c51c3be5b31bcfaa24a2362d610339a5244
LucaCDRocha <lucadrocha@gmail.com>
2024-12-09 15:00:26 +0100

    Add conditional rendering for empty state in HomeView; display message when no pets are available

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -48,22 +48,25 @@
 		<span class="loading loading-spinner loading-lg"></span>
 	</div>
 	<div v-else class="flex flex-col gap-8 justify-end items-center w-full h-full overflow-hidden pb-4">
-		<div class="stack relative w-80 h-full pt-8">
-			<SwipeableCard
-				v-for="(card, index) in cards"
-				:key="card._id"
-				:card="card"
-				:index="index"
-				@swipe="(direction) => handleSwipe(direction, card._id)" />
-		</div>
-		<div class="flex justify-between mt-1 w-72 z-50">
-			<button @click="handleSwipe('left')" class="btn btn-lg btn-error btn-circle">
-				<span class="material-symbols-outlined fill align-middle text-3xl">close</span>
-			</button>
-			<button @click="handleSwipe('right')" class="btn btn-lg btn-success btn-circle">
-				<span class="material-symbols-outlined fill align-middle text-3xl">favorite</span>
-			</button>
-		</div>
+			<div v-if="cards.length === 0" class="flex justify-center items-center h-full w-full text-center text-xl">
+				Plus d'animaux à adopter.
+			</div>
+			<div v-else class="stack relative w-80 h-full pt-8">
+				<SwipeableCard
+					v-for="(card, index) in cards"
+					:key="card._id"
+					:card="card"
+					:index="index"
+					@swipe="(direction) => handleSwipe(direction, card._id)" />
+			</div>
+			<div v-if="cards.length > 0" class="flex justify-between mt-1 w-72 z-50">
+				<button @click="handleSwipe('left')" class="btn btn-lg btn-error btn-circle">
+					<span class="material-symbols-outlined fill align-middle text-3xl">close</span>
+				</button>
+				<button @click="handleSwipe('right')" class="btn btn-lg btn-success btn-circle">
+					<span class="material-symbols-outlined fill align-middle text-3xl">favorite</span>
+				</button>
+			</div>
 	</div>
 </template>
 
