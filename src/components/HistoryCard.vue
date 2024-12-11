70351d7dc2e6cbc8dcba88f6b6c004d607de7b89
elodiePE <elodie.perring@gmail.com>
2024-12-11 14:50:00 +0100

    Add HistoryCard component and integrate it into HistoryView for displaying likes and dislikes

---
+++ src/components/HistoryCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  card: Object,
+});
+
+console.log(props.card);
+const emit = defineEmits(["remove"]);
+
+const handleFavoriteClick = () => {
+  emit("remove", props.card);
+};
+const imageSrc = computed(() => {
+  const byteArray = new Uint8Array(props.card.images[0].data.data);
+  const binaryString = byteArray.reduce(
+    (data, byte) => data + String.fromCharCode(byte),
+    ""
+  );
+  return "data:image/jpeg;base64," + btoa(binaryString);
+});
+</script>
+
+<template>
+<div class="card card-side bg-base-100 shadow-xl w-full ">
+	<figure>
+		<img :src="imageSrc" alt="Card image" class="w-28 h-28 object-cover" />
+	</figure>
+	<div class="card-body p-3">
+		<h2 class="card-title text-lg font-bold">{{ card.nom }}</h2>
+		<p class="text-sm">{{  card.spa_id.nom }}</p>
+	</div>
+	<div class="btn btn-error self-center mr-5 " @click="handleFavoriteClick">
+		<span class="material-symbols-outlined fill align-middle">close</span>
+	</div>
+	
+
+</div>
+</template>
+
+<style scoped>
+</style>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -2,72 +2,116 @@
 import { ref, onMounted } from "vue";
 import { getUserIdFromToken } from "@/utils/token";
 import { useFetchApiCrud } from "@/composables/useFetchApiCrud";
-import SmallCard from "@/components/SmallCard.vue";
+import HistoryCard from "@/components/HistoryCard.vue";
 
 const userId = getUserIdFromToken(localStorage.getItem("token"));
 const likes = ref([]);
 const dislikes = ref([]);
 const petCrudLikes = useFetchApiCrud(`users/${userId}/likes`);
 const petCrudDislikes = useFetchApiCrud(`users/${userId}/dislikes`);
-const petCrud = useFetchApiCrud('pets');
+const petCrud = useFetchApiCrud("pets");
 const isLoading = ref(false);
 
 const fetchHistory = async () => {
-	isLoading.value = true;
-	const { data: likesData, error: likesError } = await petCrudLikes.readAll({
-		Authorization: `Bearer ${localStorage.getItem("token")}`,
-	}, 'likes');
-	if (!likesError) {
-		likes.value = likesData;
-	}
+  isLoading.value = true;
+  const { data: likesData, error: likesError } = await petCrudLikes.readAll(
+    {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    "likes"
+  );
+  if (!likesError) {
+    likes.value = likesData;
+    likes.value.reverse();
+  }
 
-	const { data: dislikesData, error: dislikesError } = await petCrudDislikes.readAll({
-		Authorization: `Bearer ${localStorage.getItem("token")}`,
-	}, 'dislikes');
-	if (!dislikesError) {
-		dislikes.value = dislikesData;
-	}
-	isLoading.value = false;
+  const { data: dislikesData, error: dislikesError } =
+    await petCrudDislikes.readAll(
+      {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      "dislikes"
+    );
+  if (!dislikesError) {
+    dislikes.value = dislikesData;
+    dislikes.value.reverse();
+  }
+  isLoading.value = false;
 };
 
 const deleteLike = async (petId) => {
-    likes.value = likes.value.filter(pet => pet._id !== petId);
-	await petCrud.del(`${petId}/like`, {
-		Authorization: `Bearer ${localStorage.getItem("token")}`,
-	});
+  likes.value = likes.value.filter((pet) => pet._id !== petId);
+  await petCrud.del(`${petId}/like`, {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  });
 };
 
 const deleteDislike = async (petId) => {
-    dislikes.value = dislikes.value.filter(pet => pet._id !== petId);
-	await petCrud.del(`${petId}/dislike`, {
-		Authorization: `Bearer ${localStorage.getItem("token")}`,
-	});
+  dislikes.value = dislikes.value.filter((pet) => pet._id !== petId);
+  await petCrud.del(`${petId}/dislike`, {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  });
 };
 
 onMounted(() => {
-	fetchHistory();
+  fetchHistory();
 });
 </script>
 
 <template>
-	<div v-if="isLoading" class="flex justify-center items-center h-full w-full">
-		<span class="loading loading-spinner loading-lg"></span>
-	</div>
-	<div v-else class="flex flex-col items-center w-full h-full overflow-scroll p-4">
-		<h1 class="text-2xl font-bold text-center my-4">Historique des likes et dislikes</h1>
-		<div class="w-full md:w-1/2">
-			<h2 class="text-xl font-semibold my-2">Likes</h2>
-			<div class="flex flex-col gap-4">
-				<SmallCard v-for="(card, index) in likes" :key="card._id" :card="card" :index="index" @click="deleteLike(card._id)" />
-			</div>
-		</div>
-		<div class="w-full md:w-1/2 mt-8">
-			<h2 class="text-xl font-semibold my-2">Dislikes</h2>
-			<div class="flex flex-col gap-4">
-				<SmallCard v-for="(card, index) in dislikes" :key="card._id" :card="card" :index="index" @click="deleteDislike(card._id)" />
-			</div>
-		</div>
-	</div>
+  <div v-if="isLoading" class="flex justify-center items-center h-full w-full">
+    <span class="loading loading-spinner loading-lg"></span>
+  </div>
+  <div
+    v-else
+    class="flex flex-col items-center w-full h-full overflow-scroll p-4"
+  >
+    <h1 class="text-2xl font-bold text-center my-4">Historique</h1>
+    <div class="flex flex-col md:w-1/2 w-full gap-4">
+      <div class="collapse collapse-plus pb-5">
+        <input type="radio" name="my-accordion-3" />
+        <div
+          className="collapse-title text-xl font-medium bg-success rounded-b-2xl mb-4 "
+        >
+          Vous avez {{ likes.length }} likes
+        </div>
+        <div className="collapse-content flex flex-col w-full gap-4">
+          <p v-if="likes.length === 0">
+            Vous avez aimé {{ dislikes.length }} animaux
+          </p>
+
+          <HistoryCard
+            v-for="(card, index) in likes"
+            :key="card._id"
+            :card="card"
+            :index="index"
+            @click="deleteLike(card._id)"
+          />
+        </div>
+      </div>
+      <div className="collapse collapse-plus pb-5">
+        <input type="radio" name="my-accordion-3" defaultChecked />
+        <div
+          className="collapse-title text-xl font-medium bg-error rounded-b-2xl mb-4"
+        >
+          Vous avez {{ dislikes.length }} dislikes
+        </div>
+
+        <div className="collapse-content flex flex-col w-full gap-4">
+          <p v-if="dislikes.length === 0">
+            Vous n'avez pas aimé {{ dislikes.length }} animaux
+          </p>
+          <HistoryCard
+            v-for="(card, index) in dislikes"
+            :key="card._id"
+            :card="card"
+            :index="index"
+            @click="deleteDislike(card._id)"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
