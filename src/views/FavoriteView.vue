e37f9f57f8d828d7ff01b704f3d94c03d54d32f3
elodiePE <elodie.perring@gmail.com>
2025-01-13 16:15:00 +0100

    Add confirmation modal for removing likes in FavoriteView and HistoryView

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -16,6 +16,9 @@
 	const { isLoading } = petCrudFavorite;
 	const selectedPet = ref(null);
 	const emit = defineEmits(["remove"]);
+	const showVerifMessage = ref(false);
+	const showVerifMessagePet = ref(null);
+	const nomPet = ref("");
 
 	const fetchPets = async () => {
 		const { data, error } = await petCrudFavorite.readAll(getAuthHeaders());
@@ -38,15 +41,11 @@
 		selectedPet.value = null;
 	};
 
-	const removeCard = (card) => {
-		cards.value = cards.value.filter((c) => c._id !== card._id);
-
-		deleteLike(card._id);
-	};
-
-	const deleteLike = async (petId) => {
-		likes.value = likes.value.filter((pet) => pet._id !== petId);
-		await petCrud.del(`${petId}/like`, getAuthHeaders());
+	const deleteLike = async () => {
+		showVerifMessage.value = false;
+		cards.value = cards.value.filter((c) => c._id !== showVerifMessagePet.value._id);
+		likes.value = likes.value.filter((pet) => pet._id !== showVerifMessagePet.value._id);
+		await petCrud.del(`${showVerifMessagePet.value._id}/like`, getAuthHeaders());
 	};
 
 	const createAdoption = async (pet) => {
@@ -56,6 +55,15 @@
 		} else {
 			router.push({ name: "chat", params: { id: pet.adoptionId } });
 		}
+	};
+	const showModal = (pet) => {
+		console.log(pet);
+		nomPet.value = pet.nom;
+		showVerifMessagePet.value = pet;
+		showVerifMessage.value = true;
+	};
+	const closeModal = () => {
+		showVerifMessage.value = false;
 	};
 </script>
 
@@ -73,13 +81,29 @@
 				:card="card"
 				:index="index"
 				:forSpa="false"
-				@clickFirstButton="removeCard"
+				@clickFirstButton="showModal(card)"
 				@click="(event) => openPetDetails(card, event)"
 				@clickChatButton="createAdoption(card)" />
 		</div>
 	</div>
 
 	<OverlayPetInfos v-if="selectedPet" :pet="selectedPet" @close="closePetDetails" />
+
+	<dialog v-show="showVerifMessage" class="modal modal-open">
+		<div class="modal-box text-center">
+			<h3 class="text-lg font-bold my-4">Voulez-vous vraiment supprimer le like pour {{ nomPet }} ?</h3>
+			<p>La suppression du like entrainera à la suppression de la discussion avec {{ nomPet }}</p>
+			<div class="modal-action">
+				<div class="btn btn-error self-center mr-5" @click="deleteLike">
+					<span>Oui, supprimer</span>
+				</div>
+				<div class="btn btn-success self-center mr-5" @click="closeModal">
+					<span>Non, annuler</span>
+				</div>
+			</div>
+		</div>
+	</dialog>
 </template>
+
 
 <style scoped></style>
